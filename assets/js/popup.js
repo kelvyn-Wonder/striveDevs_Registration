2ff63d241cf3abf79b2bdaaa4147c27b89133ec6
kelvyn-Wonder <kelvinasante920@gmail.com>
2024-12-20 19:19:39 -0800

    update

---
+++ assets/js/popup.js
@@ -0,0 +1,22 @@
+function showPopup() {
+  document.getElementById('success-popup').style.display = 'flex';
+  document.body.classList.add('no-scroll'); 
+}
+
+function closePopup() {
+  
+  document.getElementById('success-popup').style.display = 'none';
+
+  
+  document.body.classList.remove('no-scroll');
+
+  document.getElementById('contact-form').reset();
+}
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  setTimeout(function() {
+      showPopup();
+  }, 500);
+});
